b2df347ea4baad4a603080973d7ceb5dea67dc6a
Turtled <trtld2@gmail.com>
2019-10-15 00:54:05 -0700

    mvp complete

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {removeItem} from "../actions/actions.js"
+import { connect } from "react-redux";
 
 const AddedFeature = props => {
+  console.log("Why do I exist?");//strange problem where AddedFeature components show up randomly in DOM (I checked everywhere and there's no <AddedFeature> components) and throw errors because they were never passed props
+  if(!props.feature) return <p>UNDEFINED</p>;//this should fix it
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick= {(e) => {console.log("onclick remove"); e.preventDefault(); props.dispatch(props.feature) }}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch: (item) => dispatch(removeItem(item))
+  }
+}
+
+export default connect(
+  null, mapDispatchToProps
+)(AddedFeature);
+
+
